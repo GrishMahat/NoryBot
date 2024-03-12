179a185c5895213b08618f796eded8dbb64d5f7d
GrishMahat <grishmahat@gmail.com>
2024-03-12 22:51:20 +0545

    1

---
+++ src/buttons/kickBtn.js
@@ -0,0 +1,14 @@
+const { PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+
+
+module.exports = {
+  customId: "kickBtn",
+  userPermissions: [],
+  botPermissions: [],
+  run: async (client, interaction) => {
+
+    const { message, channel, guildId, guild, user } = interaction;
+
+  }
+
+}
