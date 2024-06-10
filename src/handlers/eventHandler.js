b8750fa39ca1305cc165a792005c6ebd7b594137
GrishMahat <grishmahat@gmail.com>
2024-06-10 21:19:27 +0545

    fix

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-/**
- * Registers event handlers for the Discord client.
- *
- * This function iterates through all event folders in the "events" directory,
- * loads the event handlers from each file, and registers them with the provided
- * Discord client.
- *
- * The event name is determined by the name of the event folder, with the
- * exception of the "validations" folder, which is mapped to the "interactionCreate"
- * event.
- *
- * @format
- * @param {Discord.Client} client - The Discord client to register the event handlers with.
- */
-
-const path = require("path");
-const getAllFiles = require("../utils/getAllFiles");
-=======
 import path from 'path';
 import { fileURLToPath } from 'url';
 import getAllFiles from '../utils/getAllFiles.js';
->>>>>>> 8cca8a2f208c8cfde72a01dbc48df9abd2e90f85
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
