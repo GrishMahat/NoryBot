1358b52d9000bbc43fce14dcad15adf94f0e77a5
NorySight <114584253+GrishMahat@users.noreply.github.com>
2024-09-09 03:09:12 +0000

    fix

--- src/schemas/feedback.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const feedbackschema = new Schema({
-    Guild: String,
-    FeedbackChannel: String,
-});
-
-export default model('feedbackSchema', feedbackschema);
