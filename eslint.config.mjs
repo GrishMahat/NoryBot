2b4413ee6287623c611aa557143a14625c532438
GrishMahat <grishmahat@gmail.com>
2025-03-12 14:10:00 +0545

    🔧 Update ESLint configuration to adjust test file ignore pattern

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,6 +40,6 @@ export default [
 			'object-shorthand': 'error',
 			'arrow-body-style': ['error', 'as-needed'],
 		},
-		ignores: ['src/config/*', 'src/types/*', 'dist/**', 'src/__tests__/**'],
+		ignores: ['src/config/*', 'src/types/*', 'dist/**', 'src/__tests__/*'],
 	},
 ];
